3218775e30410360c3747634ceb3a1e053d829c2
Alexander Graebe <alex.graebe@gmail.com>
2019-06-16 18:02:57 +0200

    Initial Commit

---
+++ google-maps-ios-polyline/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  google-maps-ios-polyline
+//
+//  Created by Alexander Graebe on 6/16/19.
+//  Copyright © 2019 Alexander Graebe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ google-maps-ios-polylineTests/google_maps_ios_polylineTests.swift
@@ -0,0 +1,34 @@
+//
+//  google_maps_ios_polylineTests.swift
+//  google-maps-ios-polylineTests
+//
+//  Created by Alexander Graebe on 6/16/19.
+//  Copyright © 2019 Alexander Graebe. All rights reserved.
+//
+
+import XCTest
+@testable import google_maps_ios_polyline
+
+class google_maps_ios_polylineTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
